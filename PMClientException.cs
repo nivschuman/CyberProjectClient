35d7f6a61e6e234dc23b4f7fbea86f3fed51fa05
Niv Schuman <niv.schuman@gmail.com>
2024-04-05 10:06:53 +0300

    PMErrorReason enum
    added enum for reason

--- PMClientException.cs
+++ PMClientException.cs
@@ -11,7 +11,7 @@ namespace PasswordManagerClient
     {
         public SocketException SE;
         public string Details;
-        public string Reason;
+        public PMErrorReason Reason;
 
         public PMClientException(SocketException e)
         {
@@ -19,14 +19,20 @@ namespace PasswordManagerClient
 
             if(e.NativeErrorCode == 10061)
             {
-                Reason = "Connection Refused";
+                Reason = PMErrorReason.ConnectionRefused;
                 Details = "Connection refused.\r\nNo connection could be made because the target computer actively refused it. This usually results from trying to connect to a service that is inactive on the foreign host—that is, one with no server application running.";
             }
             else if(e.NativeErrorCode == 10060)
             {
-                Reason = "Connection Timeout";
+                Reason = PMErrorReason.ConnectionTimeouted;
                 Details = "Connection timed out.\r\nA connection attempt failed because the connected party did not properly respond after a period of time, or the established connection failed because the connected host has failed to respond.";
             }
         }
+    }
+
+    enum PMErrorReason
+    {
+        ConnectionRefused,
+        ConnectionTimeouted
     }
 }
